473962e554198471b71ec8c1ff995f91a5752def
Jake Phillips <jphill1314@gmail.com>
2016-05-19 16:10:36 -0400

    flags now next to country

--- app/src/main/java/com/phillips/jake/formulaschedule/ListViewDetailsAdapter.java
+++ app/src/main/java/com/phillips/jake/formulaschedule/ListViewDetailsAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -27,9 +28,57 @@ public class ListViewDetailsAdapter extends ArrayAdapter<ListViewDetails> {
 
         TextView tvCountry = (TextView) convertView.findViewById(R.id.country_name);
         TextView tvDates = (TextView) convertView.findViewById((R.id.dates));
+        ImageView flag = (ImageView) convertView.findViewById(R.id.country_flag);
 
         tvCountry.setText(detail.country);
         tvDates.setText(detail.dates);
+
+        switch(detail.country){
+            case "Australia": flag.setImageResource(R.drawable.australia);
+                break;
+            case "Austria": flag.setImageResource(R.drawable.austria);
+                break;
+            case "Europe": flag.setImageResource(R.drawable.azerbaijan);
+                break;
+            case "Bahrain": flag.setImageResource(R.drawable.bahrain);
+                break;
+            case "Belgium": flag.setImageResource(R.drawable.belgium);
+                break;
+            case "Brazil": flag.setImageResource(R.drawable.brazil);
+                break;
+            case "Canada": flag.setImageResource(R.drawable.canada);
+                break;
+            case "China": flag.setImageResource(R.drawable.china);
+                break;
+            case "England": flag.setImageResource(R.drawable.england);
+                break;
+            case "Germany": flag.setImageResource(R.drawable.germany);
+                break;
+            case "Hungary": flag.setImageResource(R.drawable.hungary);
+                break;
+            case "Italy": flag.setImageResource(R.drawable.italy);
+                break;
+            case "Japan": flag.setImageResource(R.drawable.japan);
+                break;
+            case "Malaysia": flag.setImageResource(R.drawable.malaysia);
+                break;
+            case "Mexico": flag.setImageResource(R.drawable.mexico);
+                break;
+            case "Monaco": flag.setImageResource(R.drawable.monaco);
+                break;
+            case "Russia": flag.setImageResource(R.drawable.russia);
+                break;
+            case "Singapore": flag.setImageResource(R.drawable.singapore);
+                break;
+            case "Spain": flag.setImageResource(R.drawable.spain);
+                break;
+            case "Abu Dhabi": flag.setImageResource(R.drawable.uae);
+                break;
+            case "United States": flag.setImageResource(R.drawable.usa);
+        }
+
+
+
 
         return convertView;
     }
